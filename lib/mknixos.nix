2628d2ebec86e2eb6dbad046bd8df23be4e2da69
DMJGilbert <dmjgilbert@me.com>
2023-06-07 16:45:59 +0100

    ci: disable linting

--- lib/mknixos.nix
+++ lib/mknixos.nix
@@ -1,6 +1,11 @@
 # This function creates a NixOS system.
-name:
-{ nixpkgs, home-manager, system, user, overlays, }:
+name: {
+  nixpkgs,
+  home-manager,
+  system,
+  user,
+  overlays,
+}:
 nixpkgs.lib.nixosSystem rec {
   inherit system;
 
@@ -8,7 +13,7 @@ nixpkgs.lib.nixosSystem rec {
     # Apply our overlays. Overlays are keyed by system type so we have
     # to go through and apply our system type. We do this first so
     # the overlays are available globally.
-    { nixpkgs.overlays = overlays; }
+    {nixpkgs.overlays = overlays;}
 
     ../hardware/${name}.nix
     ../machines/${name}.nix
