6dafd88ced5504d17fbf4e20d0fa46b00ef280fa
DMJGilbert <dmjgilbert@me.com>
2023-07-31 14:31:17 +0100

    fix: correct lspsaga finder command

--- users/darren/config/nvim/lua/core/plugins/lspsaga.lua
+++ users/darren/config/nvim/lua/core/plugins/lspsaga.lua
@@ -2,12 +2,13 @@ return {
 	"glepnir/lspsaga.nvim",
 	event = "LspAttach",
 	keys = {
-		{ "gs", "<cmd>Lspsaga lsp_finder<cr>" },
+		{ "gs", "<cmd>Lspsaga finder<cr>" },
 		{ "gd", "<cmd>Lspsaga goto_definition<cr>" },
 		-- { "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>" },
 		{ "gf", "<cmd>Lspsaga show_cursor_diagnostics<cr>" },
 		-- { "<leader>z", "<cmd>Lspsaga diagnostic_jump_prev<cr>" },
 		{ "<leader>a", "<cmd>Lspsaga diagnostic_jump_next<cr>" },
+		{ "<leader>A", "<cmd>Lspsaga diagnostic_jump_prev<cr>" },
 	},
 	config = function()
 		require("lspsaga").setup({
