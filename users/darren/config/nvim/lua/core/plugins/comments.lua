3ac660b7836c271ecd2bb9e02d12dc170125dafe
DMJGilbert <dmjgilbert@me.com>
2024-01-03 11:05:42 +0000

    feat: add neogen

--- users/darren/config/nvim/lua/core/plugins/comments.lua
+++ users/darren/config/nvim/lua/core/plugins/comments.lua
@@ -1,4 +1,37 @@
 return {
-	"tpope/vim-commentary",
-	event = "BufReadPre",
+	{
+		"tpope/vim-commentary",
+		event = "BufReadPre",
+	},
+	{
+		"danymat/neogen",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		event = "BufReadPre",
+		config = {
+			snippet_engine = "vsnip",
+		},
+		keys = {
+			{
+				"<leader>df",
+				function()
+					require("neogen").generate({ type = "func" })
+				end,
+				desc = "Function Annotation",
+			},
+			{
+				"<leader>dF",
+				function()
+					require("neogen").generate({ type = "file" })
+				end,
+				desc = "File Annotation",
+			},
+			{
+				"<leader>qt",
+				function()
+					require("neogen").generate({ type = "type" })
+				end,
+				desc = "Type Annotation",
+			},
+		},
+	},
 }
