7ceff0cf148b2d9a1ebdc537157915dfe404a080
DMJGilbert <dmjgilbert@me.com>
2024-01-23 11:23:11 +0000

    fix: remove cachix substituter

--- machines/shared.nix
+++ machines/shared.nix
@@ -5,11 +5,6 @@
       keep-derivations = true;
       experimental-features = ["nix-command" "flakes"];
       trusted-users = ["root" "@wheel" "@admin"];
-      substituters = ["https://nix-config.cachix.org" "https://nix-community.cachix.org"];
-      trusted-public-keys = [
-        "nix-config.cachix.org-1:Vd6raEuldeIZpttVQfrUbLvXJHzzzkS0pezXCVVjDG4="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
     };
     gc = {
       automatic = true;
